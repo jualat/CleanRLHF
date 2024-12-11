855c1e4453b4936c730acbc7785b2c114f5e62b9
jualat <--global>
2024-12-12 00:00:01 +0100

    lint

--- framework/unsupervised_exploration.py
+++ framework/unsupervised_exploration.py
@@ -1,6 +1,7 @@
 import numpy as np
 from scipy.spatial import KDTree
 
+
 class ExplorationRewardKNN:
     def __init__(self, k=3):
         self.k = k
@@ -12,7 +13,9 @@ class ExplorationRewardKNN:
         :param states: Batch of states, shape (num_envs, obs_dim)
         """
         if len(states.shape) != 2:
-            raise ValueError(f"States must have shape (num_envs, obs_dim), but got {states.shape}")
+            raise ValueError(
+                f"States must have shape (num_envs, obs_dim), but got {states.shape}"
+            )
         self.visited_states.extend(states.tolist())
 
     def compute_intrinsic_rewards(self, states):
@@ -27,12 +30,16 @@ class ExplorationRewardKNN:
 
         # Normalizing the all visited_states to ensure that each dimension contributes equally to the distance metric
         visited_states = np.array(self.visited_states)
-        visited_states = (visited_states - visited_states.mean(axis=0)) / (visited_states.std(axis=0) + 1e-8)
+        visited_states = (visited_states - visited_states.mean(axis=0)) / (
+            visited_states.std(axis=0) + 1e-8
+        )
         tree = KDTree(visited_states)
 
         # Normalizing the data to have a mean of 0 and a standard deviation of 1 allows the KDTree
         # to process all dimensions on an equal footing, irrespective of their original distributions.
-        states = (states - visited_states.mean(axis=0)) / (visited_states.std(axis=0) + 1e-8)
+        states = (states - visited_states.mean(axis=0)) / (
+            visited_states.std(axis=0) + 1e-8
+        )
 
         # Query the KDTree for distances.
         # By asking for k + 1 neighbors, we ensure that the kth actual neighbor (excluding the query point itself)
@@ -48,6 +55,8 @@ class ExplorationRewardKNN:
         # Removing NaNs and infinite values in order to ensure the system remains robust
         if not np.all(np.isfinite(intrinsic_rewards)):
             print(f"Non-finite intrinsic rewards detected: {intrinsic_rewards}")
-            intrinsic_rewards = np.nan_to_num(intrinsic_rewards, nan=0.0, posinf=1e6, neginf=-1e6)
+            intrinsic_rewards = np.nan_to_num(
+                intrinsic_rewards, nan=0.0, posinf=1e6, neginf=-1e6
+            )
 
         return intrinsic_rewards
