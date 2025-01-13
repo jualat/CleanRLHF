9aef964e55ab1054e34f1c66203b87f6e4fe506e
Benjamin Kuen <b.kuen@campus.lmu.de>
2025-01-13 15:30:38 +0100

    Fix wandb login

--- framework/slurm/start_job.sh
+++ framework/slurm/start_job.sh
@@ -1,2 +1,2 @@
 cd ~/workspace/cleanrlhf/framework
-read -s $WANDB_API_KEY && sbatch --export=WANDB_API_KEY=$WANDB_API_KEY wandb_sweep.sbatch
+read -s WANDB_API_KEY && sbatch --export=WANDB_API_KEY=$WANDB_API_KEY wandb_sweep.sbatch
