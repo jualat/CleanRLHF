371584b70ec9542764f692de6ac04fdf3622d7fd
jualat <github@alattar.de>
2025-01-30 22:56:17 +0100

    add formatter

--- toolbox/app/components/run-selector.tsx
+++ toolbox/app/components/run-selector.tsx
@@ -1,74 +1,74 @@
-import * as React from "react"
-import {ChevronsUpDown, type LucideIcon} from "lucide-react"
+import * as React from "react";
+import { ChevronsUpDown, type LucideIcon } from "lucide-react";
 
 import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuTrigger,
-} from "~/components/ui/dropdown-menu"
+	DropdownMenu,
+	DropdownMenuContent,
+	DropdownMenuItem,
+	DropdownMenuTrigger,
+} from "~/components/ui/dropdown-menu";
 import {
-  SidebarMenu,
-  SidebarMenuButton,
-  SidebarMenuItem, useSidebar,
-} from "~/components/ui/sidebar"
-import {useState} from "react";
-import {useSearchParams} from "react-router";
+	SidebarMenu,
+	SidebarMenuButton,
+	SidebarMenuItem,
+	useSidebar,
+} from "~/components/ui/sidebar";
+import { useState } from "react";
+import { useSearchParams } from "react-router";
 
 interface Run {
-  name: string
-  logo: LucideIcon
+	name: string;
+	logo: LucideIcon;
 }
 
 interface RunSelectorProps {
-  runs: Run[]
-  index: number
+	runs: Run[];
+	index: number;
 }
 
-export function RunSelector({
-  runs,
-  index,
-}: RunSelectorProps) {
-  const [searchParams, setSearchParams] = useSearchParams();
-  const run = searchParams.get(`run${index}`);
-  const [activeRun, setActiveRun] = useState(runs.find((r) => r.name === run));
+export function RunSelector({ runs, index }: RunSelectorProps) {
+	const [searchParams, setSearchParams] = useSearchParams();
+	const run = searchParams.get(`run${index}`);
+	const [activeRun, setActiveRun] = useState(runs.find((r) => r.name === run));
 
-  return (
-    <SidebarMenu>
-      <SidebarMenuItem>
-        <DropdownMenu>
-          <DropdownMenuTrigger asChild>
-            {activeRun && <SidebarMenuButton
-              size="lg"
-              className="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground"
-            >
-              <div className="flex aspect-square size-8 items-center justify-center rounded-lg bg-sidebar-primary text-sidebar-primary-foreground">
-                <activeRun.logo className="size-4" />
-              </div>
-              <div className="grid flex-1 text-left text-sm leading-tight">
-                <span className="truncate font-semibold">
-                  {activeRun.name}
-                </span>
-              </div>
-              <ChevronsUpDown className="ml-auto" />
-            </SidebarMenuButton>}
-          </DropdownMenuTrigger>
-          <DropdownMenuContent className="w-[--radix-popper-anchor-width]">
-            {runs.map((run) => (
-              <DropdownMenuItem
-                key={run.name}
-                onClick={() => {
-                  setActiveRun(run);
-                  searchParams.set(`run${index}`, run.name);
-                  setSearchParams(searchParams);
-                }}
-              >
-                <span>{run.name}</span>
-              </DropdownMenuItem>
-            ))}
-          </DropdownMenuContent>
-        </DropdownMenu>
-      </SidebarMenuItem>
-    </SidebarMenu>
-  )
+	return (
+		<SidebarMenu>
+			<SidebarMenuItem>
+				<DropdownMenu>
+					<DropdownMenuTrigger asChild>
+						{activeRun && (
+							<SidebarMenuButton
+								size="lg"
+								className="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground"
+							>
+								<div className="flex aspect-square size-8 items-center justify-center rounded-lg bg-sidebar-primary text-sidebar-primary-foreground">
+									<activeRun.logo className="size-4" />
+								</div>
+								<div className="grid flex-1 text-left text-sm leading-tight">
+									<span className="truncate font-semibold">
+										{activeRun.name}
+									</span>
+								</div>
+								<ChevronsUpDown className="ml-auto" />
+							</SidebarMenuButton>
+						)}
+					</DropdownMenuTrigger>
+					<DropdownMenuContent className="w-[--radix-popper-anchor-width]">
+						{runs.map((run) => (
+							<DropdownMenuItem
+								key={run.name}
+								onClick={() => {
+									setActiveRun(run);
+									searchParams.set(`run${index}`, run.name);
+									setSearchParams(searchParams);
+								}}
+							>
+								<span>{run.name}</span>
+							</DropdownMenuItem>
+						))}
+					</DropdownMenuContent>
+				</DropdownMenu>
+			</SidebarMenuItem>
+		</SidebarMenu>
+	);
 }
--- toolbox/app/components/video/video-card.tsx
+++ toolbox/app/components/video/video-card.tsx
@@ -1,34 +1,32 @@
-import React, {type ReactNode} from "react";
+import React, { type ReactNode } from "react";
 
 interface VideoCardProps {
-  videoSrc: string;
-  poster: string;
-  children?: ReactNode;
+	videoSrc: string;
+	poster: string;
+	children?: ReactNode;
 }
 
 export const VideoCard = ({ videoSrc, poster, children }: VideoCardProps) => {
-  return (
-    <div className="group bg-white shadow-md rounded-lg overflow-hidden hover:shadow-lg">
-      <div className="relative aspect-square">
-        <video
-          key={videoSrc}
-          className="w-full h-full object-cover"
-          controls
-          autoPlay={true}
-          loop={true}
-          muted={true}
-          // poster={poster}
-        >
-          <source src={videoSrc} type="video/mp4" />
-          Your browser does not support the video tag.
-        </video>
-        {/*<div className="absolute inset-0 bg-black bg-opacity-50 flex items-center justify-center opacity-0 group-hover:opacity-100 transition">*/}
-        {/*  <button className="text-white bg-gray-800 bg-opacity-75 px-4 py-2 rounded-lg">Play Video</button>*/}
-        {/*</div>*/}
-      </div>
-      <div className="p-4">
-        {children}
-      </div>
-    </div>
-  );
+	return (
+		<div className="group bg-white shadow-md rounded-lg overflow-hidden hover:shadow-lg">
+			<div className="relative aspect-square">
+				<video
+					key={videoSrc}
+					className="w-full h-full object-cover"
+					controls
+					autoPlay={true}
+					loop={true}
+					muted={true}
+					// poster={poster}
+				>
+					<source src={videoSrc} type="video/mp4" />
+					Your browser does not support the video tag.
+				</video>
+				{/*<div className="absolute inset-0 bg-black bg-opacity-50 flex items-center justify-center opacity-0 group-hover:opacity-100 transition">*/}
+				{/*  <button className="text-white bg-gray-800 bg-opacity-75 px-4 py-2 rounded-lg">Play Video</button>*/}
+				{/*</div>*/}
+			</div>
+			<div className="p-4">{children}</div>
+		</div>
+	);
 };
